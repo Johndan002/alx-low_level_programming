c204d70216981023eeaf47deaaceac2ff007db94
Johndan002 <john.nworah0@gmail.com>
2022-09-12 17:20:14 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry - point
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -8,7 +8,7 @@
 int main(void)
 {
 	char c;
-	
+
 	c = 'z';
 	while
 		(c >= 'a') {
