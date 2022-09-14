87aff0792a411a5c6755506a8fca0260a9571fa1
Johndan002 <john.nworah0@gmail.com>
2022-09-14 14:39:28 -0700

    Task 0..

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints putchar
- *
+ * main - print putchar
  * Return: always 0
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -18,4 +18,5 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
+
 }
