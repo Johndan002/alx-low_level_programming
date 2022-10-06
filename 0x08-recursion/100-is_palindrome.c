2fcbba259fa403429e5d5362c2c10edb4f7b47fa
Johndan002 <john.nworah0@gmail.com>
2022-10-06 14:41:30 -0700

    Task 0x08

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,9 +4,9 @@ int find_strlen(char *s);
 int check_palindrome(char *s, int len, int index);
 int is_palindrome(char *s);
 
-/**                                                                                                                                   
+/**
  *
- * find_strlen - Returns the length of a string 
+ * find_strlen - Returns the length of a string
  * @s: The string to be measured.
  *
  * Return: The length of the string.
